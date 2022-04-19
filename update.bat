e1ec8b72c6ff9481786ed382336544fded6c3533
noknowledgeman <80455435+noknowledgeman@users.noreply.github.com>
2022-04-19 14:33:44 +0200

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 @echo off
 reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
-reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "%userprofile%\how.bmp" /f 
-reg delete "HKCU\Software\Microsoft\Internet Explorer\Desktop\General" /v WallpaperStyle /f
-reg add "HKCU\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
+::reg add "HKCU\control panel\desktop" /v wallpaper /t REG_SZ /d "%userprofile%\how.bmp" /f 
+::reg delete "HKCU\Software\Microsoft\Internet Explorer\Desktop\General" /v WallpaperStyle /f
+::reg add "HKCU\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters 
